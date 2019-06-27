3474e90e83e83bc8da5cb08eea3f9c3b4ebb8d82
Robert Gallardo <rgallardo@fb.com>
2019-06-26 17:33:57 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by rgallardo on 6/26/19.
+//  Copyright © 2019 rgallardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
